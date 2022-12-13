9820a82c8b21bd3bc62bd34624278d935b69a3c5
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-13 15:53:14 +0100

    creato array con squadre

---
+++ snack-1.php
@@ -0,0 +1,52 @@
+<!-- Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+
+$partite = [
+       [
+        "squadraCasa" => "Torino",
+        "squadraOspite" => "Milano",
+        "puntiFattiCasa" => 12,
+        "puntiFattiOspite" => 24,
+       ],
+       [
+        "squadraCasa" => "Bologna",
+        "squadraOspite" => "Trento",
+        "puntiFattiCasa" => 30,
+        "puntiFattiOspite" => 40,
+       ],
+       [
+        "squadraCasa" => "Venezia",
+        "squadraOspite" => "Genova",
+        "puntiFattiCasa" => 43,
+        "puntiFattiOspite" => 28,
+       ],
+       [
+        "squadraCasa" => "Firenze",
+        "squadraOspite" => "Roma",
+        "puntiFattiCasa" => 10,
+        "puntiFattiOspite" => 36,
+       ],
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    
+
+
+</body>
+</html>
