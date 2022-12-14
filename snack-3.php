ecd8c2023b934766395bb986fe2b80b4fd0663e0
Ale-1407 <alessiacarrera07@gmail.com>
2022-12-14 09:57:45 +0100

    snack-3 completato

---
+++ snack-3.php
@@ -0,0 +1,71 @@
+<!-- Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in 
+questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post 
+associati a quella data. Stampare ogni data con i relativi post. -->
+
+
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
+$newArray = array_keys($posts);
+
+// var_dump($posts[$newArray[0]]);
+
+for( $i = 0; $i < count($posts); $i++ ){
+    echo $newArray[$i];
+    echo "<br>";
+
+    for($k = 0; $k < count( $posts[ $newArray[$i] ] ); $k++ ){
+        echo $posts[ $newArray[$i] ] [$k]['title'];
+        echo "<br>";
+        echo $posts[ $newArray[$i] ] [$k]['author'];
+        echo "<br>";
+        echo $posts[ $newArray[$i] ] [$k]['text'];
+        echo "<br>";
+        echo "<br>";
+    } 
+}
+
+
+?>
